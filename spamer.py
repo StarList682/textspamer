5d1227590fe7aa80aeeeb7b1b2b2bd4a8701d2a0
StarList <143035921+StarList682@users.noreply.github.com>
2024-07-28 19:16:39 +0400

    Update v1.1

--- spamer.py
+++ spamer.py
@@ -1,5 +1,7 @@
 import pyautogui
 import time
+import emoji
+import pyperclip
 
 def send_message(message, interval, repetitions):
     """
@@ -9,13 +11,15 @@ def send_message(message, interval, repetitions):
     :param interval: The interval between messages in seconds.
     :param repetitions: The number of times the message will be sent.
     """
+    message = emoji.emojize(message, language='alias')  # Convert emoji aliases to actual emojis
     for _ in range(repetitions):
-        pyautogui.typewrite(message)
+        pyperclip.copy(message)  # Copy the message to the clipboard
+        pyautogui.hotkey("ctrl", "v")  # Paste the message from the clipboard
         pyautogui.press("enter")
         time.sleep(interval)
 
 if __name__ == "__main__":
-    message = input("Enter the message to send: ")
+    message = input("Enter the message to send (you can use emoji aliases, e.g., :smile:): ")
     interval = float(input("Enter the interval between messages (in seconds): "))
     repetitions = int(input("Enter the number of repetitions: "))
 
